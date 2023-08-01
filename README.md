4d8d7f809964c23d3324122590d819f3fdb4fa28
Pk686 <priyanshu.kumar2020@vitstudent.ac.in>
2023-08-01 23:57:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my portfolio website made using react and styled components.
---
+++ src/data/ProjectData.js
@@ -0,0 +1,59 @@
+export const Project = [
+    {
+        id:1,
+        name:"AugiTrons",
+        description:"A marker-based augmented reality (AR) enabled online electronics purchasing web application is presented to provide users with an intuitive and interactive shopping experience.",
+        tags:["html","javascript","styledComponents","Python","Django","Bootstrap","css"],
+        
+        demo:"https://www.linkedin.com/in/priyanshu-kumar-b9ba611ba/details/projects/",
+        github:"https://github.com/Pk686/website.github.io"
+    },
+    {
+        id:2,
+        name:"  Ecommerce",
+        description:"A website that allows people to buy physical goods, digital products over the internet rather than at a brick-and-mortar location.",
+        tags:["html","javascript","styledComponents","Python","Django","Bootstrap","css"],
+        
+        demo:"https://ecommerce686.herokuapp.com/",
+        github:"https://github.com/Priyanshu1234-kumar/ecommerce"
+    },
+    {
+        id:3,
+        name:"Movieapp",
+        description:"A web application that offers overviews of movies encouraging users to read it in details.",
+        tags:["html","sass","styledComponents","javascript","Bootstrap","css","api"],
+        
+        demo:"https://moviewebsite-1a6ed.web.app/",
+        github:"https://github.com/Priyanshu1234-kumar/Movie_App"
+        
+    },
+    {
+        id:4,
+        name:"Echo-health",
+        description:"A web application that helps manage the information related to health care and manage the data related to all Departments of healthcare such as clinical, Financial laboratory.",
+        tags:["html","javascript","Python","Django","css"],
+        
+        demo:"https://github.com/Priyanshu1234-kumar/Echo-health",
+        github:"https://github.com/Priyanshu1234-kumar/Echo-health"
+    },
+    {
+        id:5,
+        name:"3d-car website",
+        description:"A cool looking 3-dimension car website. It has really smooth animations.",
+        tags:["html","javascript","css"],
+        
+        demo:"https://github.com/Priyanshu1234-kumar/carwebsite3d",
+        github:"https://github.com/Priyanshu1234-kumar/carwebsite3d"
+       
+    },
+    {
+        id:6,
+        name:"AnonyVit",
+        description:"A Blogging website founded by three fellow mates to post some unique information that engineering students must know.",
+        tags:["html","Bootstrap","css"],
+        
+        demo:"https://bloggersquad88.wixsite.com/anonyvity-1",
+        github:"https://github.com/Priyanshu1234-kumar/Anonyvit"
+        
+    },
+] 
